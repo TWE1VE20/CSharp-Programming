f47ff12ce2a61924275c29fedbbfff6414bca47a
TWE1VE20 <95527079+TWE1VE20@users.noreply.github.com>
2024-01-09 15:19:51 +0900

    feat : hello world

---
+++ 00. CSharp-Programming/00. CSharp-Programming/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _00._CSharp_Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
